9ca4228f0bf9cb84b85c2ecd8f0faa0050802002
rajmohanperiyasamy <rajmohanjr@gmail.com>
2016-06-08 16:28:32 +0530

    Update settings.py

--- snap/snap/settings.py
+++ snap/snap/settings.py
@@ -38,7 +38,7 @@ EMAIL_USE_TLS = True
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'rajmohanjr@gmail.com'
-EMAIL_HOST_PASSWORD = 'mythangapondatti'
+EMAIL_HOST_PASSWORD = '****'
 DEFAULT_FROM_EMAIL = ''
 SERVER_EMAIL = '<address>'
 
